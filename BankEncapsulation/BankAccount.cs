ed35c8a496681cdeec40d7ca0087cd01a570519e
Scott <ScottAdelman97@gmail.com>
2022-07-14 14:15:33 -0600

    simple bank account

---
+++ BankEncapsulation/BankAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankEncapsulation
+{
+    public class BankAccount
+    {
+        private double balance = 0;
+
+        public void Deposit(double inputBalanace)
+        {
+            balance += inputBalanace;
+        }
+
+        public double GetBalance()
+        {
+            return balance;
+        }
+
+        public void Withdrawl(double inputBalance)
+        {
+            balance -= inputBalance;
+        }
+    }
+        
+ }
+
+
---
+++ BankEncapsulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using BankEncapsulation;
+
+var myBankAccount = new BankAccount();
+
+Console.WriteLine($"Hello, your bank account balance is {myBankAccount.GetBalance()}");
+
+Console.WriteLine("Would you like to do a deposit or withdrawl?");
+var answer = Console.ReadLine();
+    if (answer == "withdrawl")
+    {
+        Console.WriteLine("How much would you like to withdraw?");
+        double withdraw = double.Parse(Console.ReadLine());
+        myBankAccount.Withdrawl(withdraw);
+        Console.WriteLine($"Your new balance is ${myBankAccount.GetBalance()}");
+    }
+
+    else if (answer == "deposit")
+    {
+        Console.WriteLine("How much would you like to deposit?");
+        double deposit = double.Parse(Console.ReadLine());
+        myBankAccount.Deposit(deposit);  
+        Console.WriteLine($"Your new balance is ${myBankAccount.GetBalance()}");
+    }
+
+    else
+    {
+        Console.WriteLine("No action taken, have a good day!");
+    }
